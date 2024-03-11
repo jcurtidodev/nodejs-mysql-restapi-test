45e497ec6e83bb9254771643ec3596255cd7773b
jcurtidodev <jcurtidodev@gmail.com>
2024-03-11 18:29:15 +0100

    Endpoints basicos

--- index.js
+++ index.js
@@ -2,5 +2,13 @@ import express from 'express'
 
 const app = express()
 
+app.get('/employees', (req, res) => res.send('Getting employees'))
+
+app.post('/employees', (req, res) => res.send('Creating employees'))
+
+app.put('/employees', (req, res) => res.send('Updating employees'))
+
+app.delete('/employees', (req, res) => res.send('Deleting employees'))
+
 app.listen(3000)
 console.log('Server running on port 3000')
