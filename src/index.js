5d17959054c8a820922abb58eded2d78a622b2c3
jcurtidodev <jcurtidodev@gmail.com>
2024-03-15 20:39:38 +0100

    variables de entorno + limpieza index.js -> app.js

--- src/index.js
+++ src/index.js
@@ -1,19 +1,6 @@
-import express from 'express'
-import employeesRoutes from './routes/employees.routes.js'
-import indexRoutes from './routes/index.routes.js'
+import app from './app.js'
+import { PORT } from './config.js'
 
-const app = express()
 
-app.use(express.json())
-
-app.use(indexRoutes)
-app.use('/api', employeesRoutes)
-
-app.use((req, res, next) => {
-  res.status(404).json({
-    message: 'Endopoint not found'
-  })
-})
-
-app.listen(3000)
-console.log('Server running on port 3000')
+app.listen(PORT)
+console.log('Server running on port ', PORT)
